c8e66c93c973d0e7580ca28c865dddc1aaef55c8
Sheng-Yu-Cheng <sean.sheng.yu.cheng@gmail.com>
2026-05-07 12:41:20 +0800

    redebug debug 14

--- final-round/icc2019cb/GPSDC.v
+++ final-round/icc2019cb/GPSDC.v
@@ -98,13 +98,11 @@ module CosInterpolate (
 
     `ifdef DEBUG
     always @(posedge clk) begin
-        if (state_r != S_IDLE) begin
-            $strobe("CosInterpolate [%0t] state=%0d next=%0d curr_r=%0d bit_r=%0d input_value_r=%f left_point_r=%f right_point_r%f",
-                $time, state_r, next_state_r, 
-                curr_r, bit_r, 
-                $itor(input_value_r) / 65536.0, $itor(left_point_r) / 65536.0, $itor(right_point_r) / 65536.0
-            );
-        end
+        $strobe("CosInterpolate [%0t] state=%0d next=%0d curr_r=%0d bit_r=%0d input_value_r=%f left_point_r=%f right_point_r%f",
+            $time, state_r, next_state_r, 
+            curr_r, bit_r, 
+            $itor(input_value_r) / 65536.0, $itor(left_point_r) / 65536.0, $itor(right_point_r) / 65536.0
+        );
     end
     `endif
 
